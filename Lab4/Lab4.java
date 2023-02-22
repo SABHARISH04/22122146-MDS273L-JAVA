412219480787afefcfa76cca5b9f3c8ecbc0da90
Sabharish-04 <msabarish044@gmail.com>
2023-02-22 11:58:04 +0530

    gg

---
+++ Lab4/Lab4.java
@@ -0,0 +1,24 @@
+package Lab4;
+
+import java.util.Scanner;
+
+public class Lab4 {
+    String acNumber;
+    Double balance;
+    String acName;
+    String[] transactionHist= {};
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        initializeCustomer(scanner);
+        Boolean exit = false;
+        
+        while (!exit) {
+
+            
+        }
+
+
+
+    }
+    
+}
